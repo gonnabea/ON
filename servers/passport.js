0454338fad8d11db23bfc64de3d24a0670be0c5e
Choi Jiwon <winning1993@naver.com>
2020-08-21 04:54:00 +0900

    Enable Join & local-LogIn with mySQL & passport.js

--- servers/passport.js
+++ servers/passport.js
@@ -2,37 +2,29 @@ import passport from "passport"
 import passportLocal from "passport-local"
 import db from "./models"
 
-const LocalStrategy = passportLocal.Strategy
+const LocalStrategy = passportLocal
 
 passport.serializeUser(function (user, done) {
   console.log(`srealizing: ${user}`)
   done(null, user.id)
 })
 
-passport.deserializeUser(function (user, done) {
-  console.log(`qqeqeeq: ${user}`)
-  db.User.findOne({ where: { id: user.id } })
-    .success(function (user) {
-      done(null, user)
-    })
-    .error(function (err) {
-      done(err, null)
-    })
+passport.deserializeUser(function (id, done) {
+  console.log(id)
+  db.User.findOne({ where: { id } }).then(function (user) {
+    done(null, user)
+  })
 })
 
 passport.use(
   new LocalStrategy(function (username, password, done) {
-    console.log(`username: ${username} password: ${password}`)
-    db.User.findOne({ where: { username: username } }).then(function (err, user) {
-      if (err) {
-        return done(err)
-      }
-
+    return db.User.findOne({ where: { username } }).then(function (user) {
+      console.log(user)
       if (!user) {
         return done(null, false, { message: "Incorrect username." })
       }
-      if (!user.verifyPassword(password)) {
-        return done(null, false, { message: "Incorrect password." })
+      if (user.password != password) {
+        return done(null, false)
       }
       return done(null, user)
     })
@@ -42,8 +34,7 @@ passport.use(
 // passport.use(
 //   "local",
 //   new LocalStrategy.Strategy((username, password, done) => {
-//     db.User.findOne({ where: { username } })
-//       .then((user) => {
+//     db.User.findOne({ where: { username } }).then((user) => {
 //         if (!user) {
 //           return done(null, false, { message: "Incorrect username and password. " })
 //         }
